e4be8a3918b7ccf007084abb33ffb92fa9d62161
MRhanrui <632836845@qq.com>
2018-09-07 13:38:17 +0800

    1

--- web/assets/admin/js/classify.js
+++ web/assets/admin/js/classify.js
@@ -10,26 +10,26 @@ $(
             img.hide()
         });
         $(add).on('click', function () {
-                    if(confirm('请确认操作')){
-                        $.ajax({
-                                url: '/admin.php?c=category&m=insert',
-                                success: function (data) {
-                                    if (data == 1) {
-                                        location.reload()
-                                    } else {
-                                        alert('error')
-                                    }
-                                }
+            if (confirm('请确认操作')) {
+                $.ajax({
+                        url: '/admin.php?c=category&m=insert',
+                        success: function (data) {
+                            if (data == 1) {
+                                location.reload()
+                            } else {
+                                alert('error')
                             }
-                        )
+                        }
                     }
+                )
+            }
         });
-        $(tbody).on('click','.remove', function () {
-            if(confirm('请确认操作')){
+        $(tbody).on('click', '.remove', function () {
+            if (confirm('请确认操作')) {
                 let id = $(this).closest('tr').attr('data-id');
                 $.ajax(
                     {
-                        url: '/admin.php?c=category&m=delete&id='+ id,
+                        url: '/admin.php?c=category&m=delete&id=' + id,
                         success: function (data) {
                             if (data == 1) {
                                 location.reload()
@@ -43,25 +43,24 @@ $(
             }
 
         })
-        $(tbody).on('blur','.form-control',function () {
-            if (confirm('请确认操作')){
-                let id = parseInt($(this).closest('tr').attr('data-id'));
-                console.log(id)
-                let k = $(this).attr('data-type');
-                let v = $(this).val();
-                $.ajax(
-                    {
-                        url: '/admin.php',
-                        data: {
-                            c:'category',
-                            m:'update',
-                            id:id,
-                            k: k,
-                            v:v
-                        }
+        $(tbody).on('blur', '.form-control', function () {
+            let id = parseInt($(this).closest('tr').attr('data-id'));
+            console.log(id)
+            let k = $(this).attr('data-type');
+            let v = $(this).val();
+            $.ajax(
+                {
+                    url: '/admin.php',
+                    data: {
+                        c: 'category',
+                        m: 'update',
+                        id: id,
+                        k: k,
+                        v: v
                     }
-                )
-            }
+                }
+            )
+
 
         })
     }
@@ -71,7 +70,7 @@ $(function () {
     let a = 0;
     setInterval(function () {
         a += 360;
-        $('#loading-img').css({transform : `rotate(${a}deg)`})
-    },1000)
+        $('#loading-img').css({transform: `rotate(${a}deg)`})
+    }, 1000)
 })
 
