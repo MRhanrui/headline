3ee0b7e9bebfd9b4e30005cfde502abadf952c6d
MRhanrui <632836845@qq.com>
2018-09-12 16:09:42 +0800

    1

--- controller/wechat/wechat.php
+++ controller/wechat/wechat.php
@@ -7,4 +7,14 @@ class wechat extends db{
             ->fetchAll();
         echo json_encode($feed);
     }
+    public function insert(){
+        $stmt = $this->pdo->prepare("insert into feed(name,avatar,content,time,address,images)values(?,?,?,?,?,?)");
+        $stmt->bindValue(1,$_GET['name']);
+        $stmt->bindValue(2,$_GET['avatar']);
+        $stmt->bindValue(3, $_GET['content']);
+        $stmt->bindValue(4,'8:00');
+        $stmt->bindValue(5, '太原.学府街');
+        $stmt->bindValue(6, 'http://img06.tooopen.com/images/20180831/tooopen_sl_131625162567285.jpg;http://img06.tooopen.com/images/20180824/tooopen_sl_165037503750657.jpg');
+        echo $stmt->execute();
+    }
 }
